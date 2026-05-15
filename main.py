15aa028d051f53bfc2566fd2c66f48f74b2dccb9
ErikPap <erik.papstel@tktk.ee>
2026-05-16 02:38:17 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+""" day = input("What day is it tommorow? (Workday/Weekend):")
+if day == "Workday":
+    print("I'm going to sleep, goodnight")
+elif day == "Weekend":    
+    print("One more episode of netflix!")
+else:    
+    print("Invalid input, please enter 'Workday' or 'Weekend'.") """
+
+
+#Finantsn]ustaja
+
+""" print("Tere! Ma olen finantsnõustaja. Kuidas ma saan teid aidata?") 
+print("Sinu iskilit n]ustaja ei tee emotsionaalseid oste." )
+money = float(input("Kui palju raha sul on? "))
+if money < 2500:
+    print("Sul on vähe raha, soovitan säästa ja investeerida.")
+elif money > 2500:
+    print("Sul on piisavalt raha, võid kaaluda investeerimist ja kulutamist.")
+elif money == 2500:
+    print("Sul on täpselt 2500 eurot, saad osta uue iphone sularahas")
+   """  
+
+#Sammulugeja
+
+""" goal = 10000
+steps = int(input("Sisesta oma sammude arv: "))
+percent = (steps / goal) * 100
+if percent >= 100:
+    print("Palju õnne! Sa oled saavutanud oma päevase sammueesmärgi!")
+elif percent >= 50:
+    print(f"Hea töö! Oled saavutanud {percent:.2f}% oma päevase sammueesmärgist. Jätka samas vaimus!")
+else:
+    print(f"Pea vastu! Oled saavutanud {percent:.2f}% oma päevase sammueesmärgist. Jätka kõndimist, sa saad sellega hakkama!")
+     """
